7ff29c88fea03aded5f75fe951e5492c3e930107
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-07-16 16:36:28 +0300

    w16.07.2021

--- app/src/test/app.js
+++ app/src/test/app.js
@@ -1,19 +1,10 @@
 const inputFile = document.querySelector('.input-file');
 const input = document.querySelector('.file');
 inputFile.addEventListener('click', () => {
-    console.log('test1');
     input.click();
 });
 input.addEventListener('change', () => {
-    console.log('test2');
-    console.log(input.files);
     const formData = new FormData();
-    console.log('formData: ', formData);
     const imagedata = input.files[0];
-    console.log('imagedata: ', imagedata);
     formData.append('inputname', imagedata);
-    console.log('formData2: ');
-    console.dir(formData);
-    console.log('formData3: ');
-    console.dir(formData.get('inputname'));
 });
