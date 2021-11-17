166944cf013e3f165fa54799cb8021085922a763
exp <arzuakov@yahoo.com>
2021-11-17 23:06:04 +0300

    h17.11.2021

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import path from "path"
 
 const __dirname = path.resolve();
-const PORT = process.env.PORT ?? 4000;
+//const PORT = process.env.PORT ?? 4000;
+const PORT = 4000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'static')));
