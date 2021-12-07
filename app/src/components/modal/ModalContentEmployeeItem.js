a4304855366601fb496e94b781f6bd4f3a0cb386
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-12-07 17:35:47 +0300

    w07.12.2021

--- app/src/components/modal/ModalContentEmployeeItem.js
+++ app/src/components/modal/ModalContentEmployeeItem.js
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
+import { modalDataContext } from '../../App';
+
 import "./ModalContentEmployeeItem.css";
 
 const ModalContentEmployeeItem = ({ name }) => {
+  const localModalProperties = useContext(modalDataContext);
+
+  function onClickHandler() {
+    const tempArr = localModalProperties.settings.executors.filter(item => {
+      if (item === name) return false;
+      return true;
+    })
+    localModalProperties.setSettings(prev => { return { ...prev, executors: tempArr}})
+  }
 
   return (
     <div className="modal-content-grid-settings-list-item">
-      <div className="modal-content-grid-settings-list-item-name">{ name }</div>
-      <div className="modal-content-grid-settings-list-item-del-btn"></div>
+      <div className="modal-content-grid-settings-list-item-name">{name}</div>
+      <div className="modal-content-grid-settings-list-item-del-btn" onClick={onClickHandler}></div>
     </div>
   );
 }
