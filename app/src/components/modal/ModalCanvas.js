1b6bebd9f6950aaa17ce82b54e5cd9ae6a7ff9a3
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-12-21 17:41:04 +0300

    w21.12.2021

--- app/src/components/modal/ModalCanvas.js
+++ app/src/components/modal/ModalCanvas.js
@@ -1,28 +1,43 @@
-import React, { useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { modalDataContext } from '../../App';
 import './MadalCanvas.css'
 
 const Canvas = () => {
+  const [localCanvas, setLocalCanvas] = useState()
   const canvasRef = useRef();
   const localModalProperties = useContext(modalDataContext);
 
+  function clickHandler(ctx) {
+    const img = new Image();
+    img.onload = function () {
+      console.log('width', this.width);
+      console.log('height', this.height);
 
+      const pr = 70000 / this.width;
+      const imgW = this.width / 100 * pr;
+      const imgH = this.height / 100 * pr;
+
+
+      ctx.drawImage(img, 0, (525 - imgH) /2, imgW, imgH)
+    }
+    img.src = localModalProperties.modalProperties.urlImg;
+  }
+  function clickHandler2(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(700, 525);
+    ctx.stroke();
+  }
 
   useEffect(() => {
-    canvasRef.current.getContext('2d');
-    console.log('canvasRef: ', canvasRef);
-    // const img = new Image();
-    // img.onload = function () {
-    //   canvasRef.current.drawImage(img, 0, 0)
-    // }
-    // img.src = localModalProperties.modalProperties.urlImg;  
+    setLocalCanvas(canvasRef.current.getContext('2d'));
   }, [])
 
   return (
     <div className="modal-content-grid-edit">
-      <div className='modal-content-grid-tools-left'></div>
+      <div className='modal-content-grid-tools-left' onClick={() => clickHandler(localCanvas)}></div>
       <canvas ref={canvasRef} className='modal-content-grid-canvas' width={700} height={525}></canvas>
-      <div className='modal-content-grid-tools-right'></div>
+      <div className='modal-content-grid-tools-right' onClick={() => clickHandler2(localCanvas)}></div>
     </div>
 
   );
