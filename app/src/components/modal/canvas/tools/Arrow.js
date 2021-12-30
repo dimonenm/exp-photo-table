386739cd8d8c937e246e7078b03860e4bc98f8c0
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-12-30 17:51:24 +0300

    w30.12.2021

--- app/src/components/modal/canvas/tools/Arrow.js
+++ app/src/components/modal/canvas/tools/Arrow.js
@@ -19,7 +19,7 @@ export default class Arrow extends Tool {
   mouseLeaveHandler(event) {
     this.mouseDown = false;
   }
-  
+
   mouseUpHandler(event) {
     this.mouseDown = false;
   }
@@ -39,15 +39,52 @@ export default class Arrow extends Tool {
 
   draw(x1, y1, x2, y2) {
     this.img.src = this.saved;
-    this.img.onload = () => {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
 
-      this.ctx.beginPath();
-      this.ctx.strokeStyle = '#ffffff';
-      this.ctx.moveTo(x1, y1);
-      this.ctx.lineTo(x2, y2);
-      this.ctx.stroke();
-    }
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = '#ffffff';
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.stroke();
+
+    // определение угла
+    // if (x2 - x1 >= 0)
+    //   console.log("влево");
+    // if (x2 - x1 < 0)
+    //   console.log("вправо");
+    // if (y2 - y1 >= 0)
+    //   console.log("вверх");
+    // if (y2 - y1 < 0)
+    //   console.log("вниз");
+    const a = 25;
+
+    const AB = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    const AC = 10;
+    //a2 = b2 + c2 - 2bc·cos α
+    let BC = Math.sqrt(Math.pow(AB, 2) + Math.pow(AC, 2) - (((AB * AC) * 2) * Math.cos(a)));
+
+    // a² + b² - c²
+    //     2ab
+    const b = (Math.pow(AB, 2) + Math.pow(BC, 2) - Math.pow(AC, 2) / ((AB * BC) * 2));
+    //b² + c² - a²
+    //     2cb
+    const c = (Math.pow(BC, 2) + Math.pow(AC, 2) - Math.pow(AB, 2) / ((AC * BC) * 2));
+
+    console.log('AB: ', AB);
+    console.log('AC: ', AC);
+    console.log('BC: ', BC);
+    console.log('a: ', a);
+    console.log('b: ', b);
+    console.log('c: ', c);
+    // console.log('проверка AC - ', Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2) - (((AB * BC) * 2) * Math.cos(a))));
+
+
+
+    // console.log('x1', x1, 'y1', y1);
+    // console.log('x2', x2, 'y2', y2);
+    // console.log(((180 * Math.atan((y2 - y1) / (x2 - x1))) / Math.PI));
+    // this.img.onload = () => {
+    // }
   }
 }
