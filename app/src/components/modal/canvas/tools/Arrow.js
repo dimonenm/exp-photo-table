2bf9733df57eeb1bd65aef6e11c89dda451520a4
exp <arzuakov@yahoo.com>
2022-01-11 01:42:02 +0300

    h11.01.2022

--- app/src/components/modal/canvas/tools/Arrow.js
+++ app/src/components/modal/canvas/tools/Arrow.js
@@ -64,7 +64,7 @@ export default class Arrow extends Tool {
     this.ctx.beginPath();
 
     this.ctx.strokeStyle = '#ffffff';
-    this.ctx.lineWidth = 3;
+    this.ctx.lineWidth = 2;
     this.ctx.lineCap = 'round';
 
     this.ctx.moveTo(x1, y1);
@@ -77,53 +77,6 @@ export default class Arrow extends Tool {
     this.ctx.lineTo(botx, boty);
 
     this.ctx.stroke();
-
-    // console.log(`x1 - ${x1}, y1 - ${y1}`);
-    // console.log(`x2 - ${x2}, y2 - ${y2}`);
-    // console.log(`topx - ${Math.floor(topx)}, topy - ${Math.floor(topy)}`);
-    // const AB = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    // console.log('AB: ', AB);
-    // console.log(`Math.atan2 - `, Math.atan2((y2 - y1), (x2 - x1)) * Math.PI);
-
-    // const lineangle = Math.atan2((y2 - y1), (x2 - x1));
-    // const d = 10;
-    // const angle = 25;
-    // const h = Math.abs(d / Math.cos(angle));
-
-    // var angle1 = lineangle + Math.PI + angle;
-    // var topx = x2 + Math.cos(angle1) * h;
-    // var topy = y2 + Math.sin(angle1) * h;
-
-    // определение угла
-    // if (x2 - x1 >= 0)
-    //   console.log("влево");
-    // if (x2 - x1 < 0)
-    //   console.log("вправо");
-    // if (y2 - y1 >= 0)
-    //   console.log("вверх");
-    // if (y2 - y1 < 0)
-    //   console.log("вниз");
-    // const a = 25;
-
-    // const AB = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    // const AC = 10;
-    // //a2 = b2 + c2 - 2bc·cos α
-    // let BC = Math.sqrt(Math.pow(AB, 2) + Math.pow(AC, 2) - (((AB * AC) * 2) * Math.cos(a)));
-
-    // // a² + b² - c²
-    // //     2ab
-    // const b = (Math.pow(AB, 2) + Math.pow(BC, 2) - Math.pow(AC, 2) / ((AB * BC) * 2));
-    // //b² + c² - a²
-    // //     2cb
-    // const c = (Math.pow(BC, 2) + Math.pow(AC, 2) - Math.pow(AB, 2) / ((AC * BC) * 2));
-
-    // console.log('AB: ', AB);
-    // console.log('AC: ', AC);
-    // console.log('BC: ', BC);
-    // console.log('a: ', a);
-    // console.log('b: ', b);
-    // console.log('c: ', c);
-    // console.log('проверка AC - ', Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2) - (((AB * BC) * 2) * Math.cos(a))));
 
   }
 }
