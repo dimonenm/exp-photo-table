d7fba8d6a6dcb160d771040985c738763e7b7bcc
exp <arzuakov@yahoo.com>
2022-03-02 01:50:04 +0300

    h02.03.2022

--- app/src/entities/WordDocument.js
+++ app/src/entities/WordDocument.js
@@ -1,5 +1,29 @@
+import { Document, Packer, Paragraph, Header, Footer, TextRun, AlignmentType, PageNumber, ImageRun } from "docx";
+import { saveAs } from "file-saver";
 export default class WordDocument{
   constructor() {
-    
+    this.title = '';
+    this.sections = [];
+  }
+  // функции доступа к полям
+  getTitle() {
+    return this.title;
+  }
+  getSections() {
+    return this.sections;
+  }
+  // функции изменения полей
+  setTitle(value) {
+    this.title = value;
+  }
+  setSections(value) {
+    this.sections = value;
+  }
+  // служебные функции
+  saveDocument(value, fileName) {
+    Packer.toBlob(value).then(blob => {
+      saveAs(blob, fileName);
+      console.log("Document created successfully");
+    });
   }
 }
