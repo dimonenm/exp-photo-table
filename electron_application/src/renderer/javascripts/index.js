4abc65f2e0af050a3f01173f0d4b7670dd5b0094
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-05-17 17:39:30 +0300

    del

--- electron_application/src/renderer/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-require('application.css')
-
-window.MessagesAPI.onLoaded((_, data) => {
-  document.getElementById('title').innerHTML = data.appName + ' App'
-  document.getElementById('details').innerHTML = 'built with Electron v' + data.electronVersion
-  document.getElementById('versions').innerHTML = 'running on Node v' + data.nodeVersion + ' and Chromium v' + data.chromiumVersion
-})
