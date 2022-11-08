5a15557f7ded041f85de38733f7fa6ee632327e4
Russs11 <russs.khalilov@gmail.com>
2022-11-08 15:30:04 +0300

    added_PreviewTitlePage_style_08.11.2022

--- electron_app/src/renderer/javascripts/components/main/PreviewTitlePage.js
+++ electron_app/src/renderer/javascripts/components/main/PreviewTitlePage.js
@@ -4,12 +4,12 @@ function PreviewTitlePage() {
     return (
         <div className='preview-title-page'>
             <div className='preview-title-page-header'>МИНИСТЕРСТВО ВНУТРЕННИХ ДЕЛ<br />ПО РЕСПУБЛИКЕ КРЫМ<br />ЭКСПЕРТНО-КРИМИНАЛИСТИЧЕСКИЙ ЦЕНТР</div>
-            <div className='preview-title-page-regular'>295048, г. Симферополь, ул. Балаклавская, 68________тел.3652 733-730</div>
+            <div className='preview-title-page-adres'>295048, г. Симферополь, ул. Балаклавская, 68________тел.3652 733-730</div>
             <div className='preview-title-page-title'>ФОТОТАБЛИЦА</div>
-            <div className='preview-title-page-regular'>к протоколу осмотра места происшествия от 10.03.2022  по факту кражи имущества по адресу: г. Симферополь, ул. Балаклавская 68" </div>
+            <div className='preview-title-page-description'>к протоколу осмотра места происшествия от 10.03.2022  по факту кражи имущества по адресу: г. Симферополь, ул. Балаклавская 68" </div>
             <div className='preview-title-page-img'></div>
-            <div className='preview-title-page-regular'>Фото № 1. Общий вид.</div>
-            <div className='preview-title-page-regular executor'>специалист___________Р.Н. Халилов</div>
+            <div className='preview-title-page-photo-description'><strong>Фото № 1.</strong> Общий вид.</div>
+            <div className='preview-title-page-executor'>специалист___________Р.Н. Халилов</div>
         </div>
     );
 }
