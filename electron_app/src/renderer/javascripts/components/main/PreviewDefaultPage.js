a04b47767fa60845d20f399fb4123088455ff1f2
Russs11 <russs.khalilov@gmail.com>
2022-11-24 17:01:42 +0300

    added_note_to_previewDefoltPage_24.11.2022

--- electron_app/src/renderer/javascripts/components/main/PreviewDefaultPage.js
+++ electron_app/src/renderer/javascripts/components/main/PreviewDefaultPage.js
@@ -32,23 +32,24 @@ function PreviewDefaultPage({number, index, img, text, photoTableData, settings,
     }
     imagesArr.push((
         <>
-            <div className='preview-default-page-img' onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
-            <div className='preview-default-page-photo-description'><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
+            <div className="preview-default-page-img" onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+            <div className="preview-default-page-photo-description"><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
         </>))
     if (index[1] && img[1]) {
         imagesArr.push((
             <>
-                <div className='preview-default-page-img' onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[1]})` }}></div>
-                <div className='preview-default-page-photo-description'><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div>
+                <div className="preview-default-page-img" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[1]})` }}></div>
+                <div className="preview-default-page-photo-description"><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div>
             </>))
 }
     
     return (
-        <div className='preview-default-page'>
-            <div className='preview-default-page-number'>{number}</div>
+        <div className="preview-default-page">
+            <div className="preview-default-page-number">{number}</div>
             {imagesArr}
-            <div className='preview-default-page-executor'>{`специалист___________${settings.executors[0]}`}</div>
-        </div>
+            <div className="preview-default-page-note">{settings.note }</div>
+            <div className="preview-default-page-executor">{`специалист___________${settings.executors[0]}`}</div>
+        </div> 
     );
 }
 
