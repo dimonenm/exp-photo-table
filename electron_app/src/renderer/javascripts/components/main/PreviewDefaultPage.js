fb6b9db3cb4de40e4acd0b5fbe2c88ad103159a9
Russs11 <russs.khalilov@gmail.com>
2022-11-25 16:59:48 +0300

    modified_PreviePages_25.11.2022

--- electron_app/src/renderer/javascripts/components/main/PreviewDefaultPage.js
+++ electron_app/src/renderer/javascripts/components/main/PreviewDefaultPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function PreviewDefaultPage({ number, index, orientation, img, text, photoTableData, settings, setModalProperties }) {
+    console.log(orientation)
+    
     const imagesArr = []
     const dbClickFirstImgHandler = (event) => {
         event.preventDefault();
@@ -34,7 +36,7 @@ function PreviewDefaultPage({ number, index, orientation, img, text, photoTableD
     if (index[0] && img[0] && orientation[0] === 'vertical') {
         imagesArr.push((
             <>
-                <div className="preview-default-page-img-vertical" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+                <div className="preview-default-page-img-vertical" onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
                 <div className="preview-default-page-photo-description-vertical"><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
                 {/* <div className="preview-default-page-note">{settings.note}</div> */}
             </>))
@@ -42,8 +44,24 @@ function PreviewDefaultPage({ number, index, orientation, img, text, photoTableD
     if (index[0] && img[0] && orientation[0] === 'horizontal') {
         imagesArr.push((
             <>
-                <div className="preview-default-page-img" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+                <div className="preview-default-page-img" onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
                 <div className="preview-default-page-photo-description"><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
+                {/* <div className="preview-default-page-note">{settings.note}</div> */}
+            </>))
+    }
+    if (index[0] && img[0] && orientation[0] === '6X9' && index[1] && img[1] && orientation[1] !== '6X9') {
+        imagesArr.push((
+            <>
+                <div className="preview-default-page-img-6X9" onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+                <div className="preview-default-page-photo-description-6X9"><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
+                {/* <div className="preview-default-page-note">{settings.note}</div> */}
+            </>))
+    }
+    if (index[0] && img[0] && orientation[0] === '9X6') {
+        imagesArr.push((
+            <>
+                <div className="preview-default-page-img-9X6" onDoubleClick={dbClickFirstImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+                <div className="preview-default-page-photo-description-9X6"><span>{`Фото № ${index[0]}. `}</span>{text[0]}</div>
                 {/* <div className="preview-default-page-note">{settings.note}</div> */}
             </>))
     }
@@ -63,6 +81,39 @@ function PreviewDefaultPage({ number, index, orientation, img, text, photoTableD
                 {/* <div className="preview-default-page-note">{settings.note}</div> */}
             </>))
     }
+    if (index[1] && img[1] && orientation[1] === '6X9' && index[0] && img[0] && orientation[0] !== '6X9') {
+        imagesArr.push((
+            <>
+                <div className="preview-default-page-img-6X9" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[1]})` }}></div>
+                <div className="preview-default-page-photo-description-6X9"><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div>
+                {/* <div className="preview-default-page-note">{settings.note}</div> */}
+            </>))
+    }
+    if (index[1] && img[1] && orientation[1] === '9X6') {
+        imagesArr.push((
+            <>
+                <div className="preview-default-page-img-9X6" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[1]})` }}></div>
+                <div className="preview-default-page-photo-description-9X6"><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div>
+                {/* <div className="preview-default-page-note">{settings.note}</div> */}
+            </>))
+    }
+    if (index[0] && img[0] && orientation[0] === '6X9' && index[1] && img[1] && orientation[1] === '6X9') {
+        imagesArr.push((
+            <>
+                <div className="preview-default-page-img-6X9-two">
+                <div className="preview-default-page-img-6X9" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[0]})` }}></div>
+                {/* <div className="preview-default-page-photo-description-6X9"><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div> */}
+                <div className="preview-default-page-img-6X9" onDoubleClick={dbClickSecondImgHandler} style={{ backgroundImage: `url(${img[1]})` }}></div>
+                {/* <div className="preview-default-page-photo-description-6X9"><span>{`Фото № ${index[1]}. `}</span>{text[1]}</div> */}
+                </div>
+                {/* <div className="preview-default-page-note">{settings.note}</div> */}
+            </>))
+        console.log('two behind')
+    }
+
+
+
+
 
     return (
         <div className="preview-default-page">
