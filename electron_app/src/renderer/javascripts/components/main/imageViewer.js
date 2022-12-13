08cb75a8c7d1cc8090995678ec9cbdd8d668b3e7
Russs11 <russs.khalilov@gmail.com>
2022-12-13 17:15:47 +0300

    added_image-viewer_component_13.12.2022

---
+++ electron_app/src/renderer/javascripts/components/main/imageViewer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function imageViewer(galleryImages, index, text) {
+
+
+
+
+    return (
+        <>
+            <div className='image-viewer'>
+                <img className='image-viewer-img' onDoubleClick={dbClickHandler} src={galleryImages[0].getUrl()} style={imgStyle}></img>
+            </div>
+            <div className='image-viewer-img-description'>
+                <span>{`Фото № ${index}. `}</span>{text}</div>
+        </>
+    );
+}
+export default imageViewer
