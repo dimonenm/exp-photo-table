4c3bc4fd378fab7b66f932d4760a8e7baf3b1a62
Russs11 <russs.khalilov@gmail.com>
2022-12-13 17:26:03 +0300

    added_image-viewer_component_13.12.2022

--- electron_app/src/renderer/javascripts/components/main/imageViewer.js
+++ electron_app/src/renderer/javascripts/components/main/imageViewer.js
@@ -9,7 +9,7 @@ function imageViewer(galleryImages, index, text) {
     return (
         <>
             <div className='image-viewer'>
-                <img className='image-viewer-img' onDoubleClick={dbClickHandler} src={galleryImages[0].getUrl()} style={imgStyle}></img>
+                <img className='image-viewer-img' src={galleryImages[0].getUrl()} ></img>
             </div>
             <div className='image-viewer-photo-description'>
                 <span>{`Фото № ${index}. `}</span>{text}</div>
