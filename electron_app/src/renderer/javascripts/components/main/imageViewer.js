38bb8e49e2630ca05ef978bcbe1f716c3fc28308
Russs11 <russs.khalilov@gmail.com>
2022-12-16 17:29:15 +0300

    added_to_ImageViewer_9X6_orientation_16.12.2022

--- electron_app/src/renderer/javascripts/components/main/imageViewer.js
+++ electron_app/src/renderer/javascripts/components/main/imageViewer.js
@@ -47,11 +47,14 @@ function ImageViewer({ img, setModalProperties }) {
 
     return (
         <>
-            <div className='image-viewer'>
-                <img className='image-viewer-photo' onDoubleClick={dbClickHandler} src={img.getUrl()} style={imgStyle} ></img>
+            <div className='image-viewer-wrapper'>
+                <div className='image-viewer'>
+                    <img className='image-viewer-photo' onDoubleClick={dbClickHandler} src={img.getUrl()} style={imgStyle} ></img>
+                </div>
+                <div className='image-viewer-photo-description'>
+                    <span style={marginForSpan}>{`Фото № ${img.getIndex()}. `} </span>{img.getImgDesc()}</div>
             </div>
-            <div className='image-viewer-photo-description'>
-                <span style={marginForSpan}>{`Фото № ${img.getIndex()}. `} </span>{img.getImgDesc()}</div>
+
         </>
     );
 }
