5e4ce5fc4103ece29e041a141386ed06d04fd001
Russs11 <russs.khalilov@gmail.com>
2023-01-13 09:38:19 +0300

    added_scaleChangerForPreviewPage 13.01.2023

--- electron_app/src/renderer/javascripts/components/main/ScaleChanger.js
+++ electron_app/src/renderer/javascripts/components/main/ScaleChanger.js
@@ -13,13 +13,31 @@ function ScaleChanger({ workPlaceStyle, setWorkPlaceStyle, previewPageScale, set
             margin: '225px 0 0 0'
         })
     }
+    const changeScaleClickHandler75 = (event) => {
+        setWorkPlaceStyle({
+            flexBasis: '420px'
+        })
+        setPreviewPageScale({
+            transform: 'scale(1.3) translate(0px, -115px)',
+            margin: '140px 0 0 0'
+        })
+    }
+    const changeScaleClickHandler50 = (event) => {
+        setWorkPlaceStyle({
+            flexBasis: '340px'
+        })
+        setPreviewPageScale({
+            transform: 'scale(1) translate(0px, 0px)',
+            margin: '10px 0 0 0'
+        })
+    }
 
 
     return (
         <div className="scale-changer">
             <div className="scale-changer-wrapper">
-                <div className="scale-changer-50" >50</div>
-                <div className="scale-changer-75" >75</div>
+                <div className="scale-changer-50" onClick={changeScaleClickHandler50}>50</div>
+                <div className="scale-changer-75" onClick={changeScaleClickHandler75}>75</div>
                 <div className="scale-changer-100" onClick={changeScaleClickHandler100}>100</div>
             </div>
         </div>
