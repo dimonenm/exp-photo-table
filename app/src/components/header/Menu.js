51b7fc6e7e9dfad66d14737a2c2623d0794ca79a
exp <arzuakov@yahoo.com>
2023-01-23 23:06:39 +0300

    h23.01.2023

--- app/src/components/header/Menu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./Menu.css";
-
-const Menu = ({ children }) => (
-    <div className="menu">{children}</div>
-);
-
-export default Menu;
--- app/src/components/header/MenuBtns.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./MenuBtns.css";
-
-const MenuBtns = ({ children }) => (
-    <div className="menu-btns">{children}</div>
-);
-
-export default MenuBtns;
