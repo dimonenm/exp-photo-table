e998d568dcf6cf2fd2c4cbdd8a8c96da19cfa7f1
exp <arzuakov@yahoo.com>
2023-01-30 23:30:47 +0300

    h30.01.2023

--- electron_app/src/renderer/javascripts/components/main/Workplace.js
+++ electron_app/src/renderer/javascripts/components/main/Workplace.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 
 const Workplace = ({ children, workPlaceStyle }) => {
-  
     return (
 
         <div className="workplace" style={workPlaceStyle}>
