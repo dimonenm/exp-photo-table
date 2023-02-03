d6c43df44ee75fb91bf022145ee952d8d7826265
Russs11 <russs.khalilov@gmail.com>
2023-02-03 13:01:25 +0300

    added_new_orientation_btns_03.02.2023

--- electron_app/src/main/application/index.js
+++ electron_app/src/main/application/index.js
@@ -40,7 +40,7 @@ export default class PhotoTableApp {
       this.window = null
     })
 
-    // this.window.webContents.openDevTools({ mode: 'detach' });
+    this.window.webContents.openDevTools({ mode: 'detach' });
   }
 
   subscribeForAppEvents() {
