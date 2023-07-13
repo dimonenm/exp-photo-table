bdba622c00552b009605afb2f8232b3dccc67100
arzuakov@yahoo.com <arzuakov@yahoo.com>
2023-07-13 11:23:28 +0300

    w13.07.2023

--- electron_app-ts/src/renderer/App.tsx
+++ electron_app-ts/src/renderer/App.tsx
@@ -46,7 +46,7 @@ export const App = (): JSX.Element => {
 
     const filePath = await window.electronAPI.openFile()
 
-    // let arrImgs: JSX.Element[] = []
+
 
     // for (const item of filePath) {
     //   const blob = new Blob([item])
@@ -57,6 +57,8 @@ export const App = (): JSX.Element => {
 
     const mimeType = 'image/png';
 
+
+
     const convertBlobToBase64Async = (blob: Blob, mimeType: string) => {
       return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -64,7 +66,6 @@ export const App = (): JSX.Element => {
           const dataUrlPrefix = `data:${mimeType};base64,`;
           const base64WithDataUrlPrefix = reader.result as string;
           const base64 = dataUrlPrefix + base64WithDataUrlPrefix.split(',')[1]
-          console.log('base64: ', base64);
           resolve(base64);
         };
         reader.onerror = reject;
@@ -75,16 +76,35 @@ export const App = (): JSX.Element => {
     const arrBase64 = await filePath.map(async (item) => {
       const blob = new Blob([item])
       return await convertBlobToBase64Async(blob, mimeType).then((base64: string) => {
+
         return base64
       });
     })
-    const elements = arrBase64.map((item, index) => {
-      const str = item as unknown
-      const elem = <img key={index} src={str as string} width={150} height={216} ></img>
-      return elem
+
+
+
+    // const elements = await arrBase64.map((item, index) => {
+    //   const str = item as unknown
+    //   const elem = <img key={index} src={str as string} width={150} height={216} ></img>
+    //   return elem
+    // })
+
+    const arrImgs: JSX.Element[] = []
+    console.log('arrImgs1: ', arrImgs);
+    
+    console.log('1');
+    const elements = await filePath.map(async (item, index) => {
+      const blob = new Blob([item])
+      const str: unknown = await blob.text
+      arrImgs.push(<img key={index} src={str as string} width={150} height={216} ></img>)
+      // const elem = <img key={index} src={str as string} width={150} height={216} ></img>
+      console.log('2');
+      // return elem
     })
-    console.log('elements: ', elements);
-    setImgs(elements)
+    console.log('arrImgs2: ', arrImgs);
+    console.log('3');
+
+    setImgs(arrImgs)
 
 
     // const urlLinks = filePath.map((item) => {
@@ -102,34 +122,6 @@ export const App = (): JSX.Element => {
     // setImgs(arrImgs)
     setIsLoading(false)
 
-    // const base64_arraybuffer = async (data: Uint8Array) => {
-    //   // Use a FileReader to generate a base64 data URI
-    //   const base64url = await new Promise<string>((res) => {
-    //     const reader = new FileReader()
-    //     reader.onload = () => {
-    //       res(reader.result as string)
-    //     }
-    //     reader.readAsDataURL(new Blob([data]))
-    //   })
-
-    //   /*
-    //   The result looks like 
-    //   "data:application/octet-stream;base64,<your base64 data>", 
-    //   so we split off the beginning:
-    //   */
-    //   return base64url.substring(base64url.indexOf(',') + 1)
-    // }
-    // const imgData = btoa(String.fromCharCode.apply(null, filePath))
-    // const imgData = 'data:image/jpeg;base64,' + await base64_arraybuffer(filePath)
-    // console.log('imgData: ', imgData);
-    // const imgsArr: JSX.Element[] = []
-
-    // for (const item of filePath) {
-    //   const imgData = 'data:image/png;base64,' + item
-    //   imgsArr.push(<img key={filePath.length} src={imgData} width={200} height={100}></img>)
-    // }
-
-    // setImgs(() => { return imgsArr })
   }
 
   return (
