11b165a839d0dc4fa8ea7ac389477a60e6628859
arzuakov@yahoo.com <arzuakov@yahoo.com>
2026-02-11 17:03:16 +0300

    w11.02.2026

--- tauri_app_react_ts/src/App.tsx
+++ tauri_app_react_ts/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import { invoke } from "@tauri-apps/api/core"
+import "./fonts.css"
 import "./App.css"
 
 function App() {
