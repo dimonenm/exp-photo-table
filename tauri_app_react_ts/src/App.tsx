2c283e319657239fbf0797431409e34f3ebd40b5
arzuakov@yahoo.com <arzuakov@yahoo.com>
2026-02-12 17:04:14 +0300

    w12.02.2026

--- tauri_app_react_ts/src/App.tsx
+++ tauri_app_react_ts/src/App.tsx
@@ -5,76 +5,13 @@ import "./fonts.css"
 import "./App.css"
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState<string>("")
-  const [name, setName] = useState<string>("")
-  const [dirMsg, setDirMsg] = useState<string>("")
-  const [dirName, setDirName] = useState<string>("")
-  const [fileName, setFileName] = useState<string>("")
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }))
-  }
-
-  async function isDir() {
-    setDirMsg(await invoke("create_exp_photo_table_dir_command", { url: dirName, fileName: fileName }))
-  }
+  // async function isDir() {
+  //   setDirMsg(await invoke("create_exp_photo_table_dir_command", { url: dirName, fileName: fileName }))
+  // }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          isDir()
-        }}
-      >
-        <input
-          id="greet-input2"
-          onChange={(e) => setDirName(e.currentTarget.value)}
-          placeholder="Enter a url..."
-        />
-        <input
-          id="greet-input3"
-          onChange={(e) => setFileName(e.currentTarget.value)}
-          placeholder="Enter a file name..."
-        />
-        <button type="submit">Enter URL</button>
-      </form>
-      <p>{dirMsg}</p>
-
-    </main>
+    
   )
 }
 
---
+++ tauri_app_react_ts/src/assets/containers/Container.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Container = ({ children:  }): JSX.Element => (
+    <div className="top-container">
+        {children}
+    </div>
+);
+
+export default Container;
