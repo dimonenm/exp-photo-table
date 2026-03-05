0d7fcd2b47c1bc27f91f430170bc17e8a0714286
arzuakov@yahoo.com <arzuakov@yahoo.com>
2026-03-05 16:50:19 +0300

    w05.03.2026

--- tauri_app_react_ts/src/assets/components/header/MenuItem.tsx
+++ tauri_app_react_ts/src/assets/components/header/MenuItem.tsx
@@ -8,22 +8,24 @@ interface MenuItemProps {
 	children: React.ReactNode
 }
 
+function selectButtonType(type: string): string {
+	switch (type) {
+		case 'menu-item download-photo-files':
+			return 'download-photo-files'
+		default:
+			return 'menu-item'
+	}
+}
+
 const MenuItem = ({ type, setDownloadedImages, children }: MenuItemProps): React.JSX.Element => {
 
-	const buttonType = (type: string): string => {
-		switch (type) {
-			case 'menu-item download-photo-files':
-				return 'download-photo-files'
-			default:
-				return 'menu-item'
-		}
-	}
+	const buttonType = selectButtonType(type)
 
 
 
 
 	return (
-		<div className={buttonType(type)}>
+		<div className={buttonType}>
 			<input type="file" className="file" multiple={true} accept="image/*"></input>
 			<a href="/" onClick={loadImgs}>{children}</a>
 		</div>
