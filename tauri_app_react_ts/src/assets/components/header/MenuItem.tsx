3e65e626a8db7f59c341c970ba3eba121f38da91
arzuakov@yahoo.com <arzuakov@yahoo.com>
2026-03-10 17:44:02 +0300

    w10.03.2026

--- tauri_app_react_ts/src/assets/components/header/MenuItem.tsx
+++ tauri_app_react_ts/src/assets/components/header/MenuItem.tsx
@@ -4,11 +4,11 @@ import "./MenuItem.css"
 // Определяем интерфейс пропсов
 interface MenuItemProps {
 	type: string,
-	setDownloadedImages: Dispatch<SetStateAction<undefined>>,
+	setDownloadedImages: Dispatch<SetStateAction<string[]>>,
 	children: React.ReactNode
 }
 
-function selectButtonType(type: string): string {
+function selectButtonStyle(type: string): string {
 	switch (type) {
 		case 'menu-item download-photo-files':
 			return 'download-photo-files'
@@ -19,13 +19,43 @@ function selectButtonType(type: string): string {
 
 const MenuItem = ({ type, setDownloadedImages, children }: MenuItemProps): React.JSX.Element => {
 
-	const buttonType = selectButtonType(type)
+		const loadImgs = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault()
+		
+		// Создаём скрытый input и кликаем по нему
+		const input = document.createElement('input')
+		input.type = 'file'
+		input.multiple = true
+		input.accept = 'image/*'
+		
+		input.onchange = (event) => {
+			const files = (event.target as HTMLInputElement).files
+			if (!files) return
 
+			const readers: Promise<string>[] = []
 
+			for (let i = 0; i < files.length; i++) {
+				const file = files[i]
+				const reader = new Promise<string>((resolve) => {
+					const reader = new FileReader()
+					reader.onload = (e) => resolve(e.target?.result as string)
+					reader.readAsDataURL(file)
+				})
+				readers.push(reader)
+			}
 
+			Promise.all(readers).then((images) => {
+				setDownloadedImages((prev) => [...prev, ...images])
+			})
+		}
+
+		input.click()
+	}
+
+	const buttonStyle = selectButtonStyle(type)
 
 	return (
-		<div className={buttonType}>
+		<div className={buttonStyle}>
 			<input type="file" className="file" multiple={true} accept="image/*"></input>
 			<a href="/" onClick={loadImgs}>{children}</a>
 		</div>
